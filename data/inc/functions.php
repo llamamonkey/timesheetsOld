2e4152eeaf890c18636b56e5acb50cc30cbb0c2c
Chris <kipper19952000-stuff@yahoo.co.uk>
2015-05-01 13:23:29 +0100

    Fixed HTTPS Detection

--- data/inc/functions.php
+++ data/inc/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function requireSSL(){
-	if($_SERVER["HTTPS"] != "on")
+	if($_SERVER["HTTPS"] != "on" && $_SERVER["HTTPS"] != "1" && $_SERVER["HTTPS"] != 1)
 	{
 		header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
 		exit();
