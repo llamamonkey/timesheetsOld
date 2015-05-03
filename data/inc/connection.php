9d23a42e24860509c7378745e399e5567443df2e
Chris <kipper19952000-stuff@yahoo.co.uk>
2015-05-03 16:24:16 +0100

    Updates connections

--- data/inc/connection.php
+++ data/inc/connection.php
@@ -3,9 +3,10 @@
 $servername = getenv("OPENSHIFT_MYSQL_DB_HOST");
 $username = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
 $password = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+$dbname = "timesheets";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
