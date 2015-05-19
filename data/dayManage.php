066dfbfed88fe90a0f95e13587bf90b3f6f22833
Chris <kipper19952000-stuff@yahoo.co.uk>
2015-05-19 08:14:33 +0100

    Added days add.edit

---
+++ data/dayManage.php
@@ -0,0 +1,96 @@
+<?php
+	
+ini_set('display_errors',1);  
+error_reporting(E_ALL);
+
+session_start();
+
+require "inc/functions.php";
+require "inc/connection.php";
+
+if (isset($_GET["day"])){
+	$currDay = $_GET["day"];
+} else {
+	$currDay = date();
+}
+
+if (isset($_GET['postKey']) || isset($_SESSION["userID"])){
+	//User information has been provided
+	$userid = 0;
+	
+	//Get userid if post key is not provided
+	if (isset($_SESSION["userID"])){
+		$userid = $_SESSION["userID"];
+	} else{
+		$sqlStr = "SELECT * FROM tblUser WHERE postKey = '" . $_GET['postKey'] . "'";
+		
+		$result = $conn->query($sqlStr);
+
+		if ($result->num_rows > 0) {
+		  	// Valid details
+			$userid = $row["userID"];
+		} else {
+			echo "Post Key didn't match a user";
+		}
+	}
+	
+	if ($userid != 0){
+		//Check if current day exists, if not create a new one
+		$sqlStr = "SELECT * FROM tblTime WHERE userID = '" . $userid . "' AND date = '" . $currDay . '"';
+		
+		$result = $conn->query($sqlStr);
+
+		if ($result->num_rows > 0) {
+		  	// Day exists
+			$dayUpWhere = "";
+			
+			if (isset($_GET["startTime"])){
+				$dayUpWhere = " startTime = '" . $_GET["startTime"] . "'";
+			}
+			
+			if (isset($_GET["endTime"])){
+				if ($dayUpWhere == ""){
+					$dayUpWhere = " endTime = '" . $_GET["endTime"] . "'";
+				} else {
+					$dayUpWhere .= ", endTime = '" . $_GET["endTime"] . "'";
+				}				
+			}
+			
+			$sqlStr = "UPDATE tblTime SET " . $dayUpWhere . "WHERE userID = " . $userid;
+			
+			if ($conn->query($sqlStr) === TRUE) {
+ 			   echo json_encode("success");
+			} else {
+    			echo json_encode($conn->error);
+			}
+		} else {
+			//Day doesn't exists so create a new one'
+		  	// Day exists
+			$dayInsField = "";
+			$dayInsVal = "";
+			
+			if (isset($_GET["startTime"])){
+				$dayInsField .= ", startTime" ;
+				$dayInsVal .= ", '" . $_GET["startTime"] . "'";
+			}
+			
+			if (isset($_GET["endTime"])){
+				$dayInsField .= ", endTime '";
+				$dayInsVal .= ", '" . $_GET["endTime"] . "'";
+			}
+			
+			$sqlStr = "INSERT INTO tblTime (userID, date, ".$dayInsField.") VALUES ('".$currDay."', ".$userid.", ".$dayInsVal.")";
+			
+			if ($conn->query($sqlStr) === TRUE) {
+ 			   echo json_encode("success");
+			} else {
+    			echo json_encode($conn->error);
+			}
+		}
+		
+	}
+} else {
+	echo "No user information provided";
+}
+
+?>
--- data/login.php
+++ data/login.php
@@ -52,11 +52,7 @@ if (isset($_POST["submit"])){
 		}
 	}
 } else {
-	//HTML Form 
-	
-	if (isset($_SESSION["userid"])){
-		echo $_SESSION["userid"];
-	}?>
+	//HTML Form ?>
 	<form action="" name="loginForm" method="POST">
 		Username: <input type="text" name="username" /><br/>
 		Password: <input type="password" name="password" /><br/>
