0b6d8b82cb9b5f311f66ceb7314986d3eb6b5c8d
Chris <kipper19952000-stuff@yahoo.co.uk>
2015-05-28 08:22:22 +0100

    Removed test echo

--- data/userInfo.php
+++ data/userInfo.php
@@ -7,7 +7,7 @@ require "inc/connection.php";
 
 if (isset($_SESSION["userID"])){
 	$sqlStr = "SELECT * FROM tblUsers WHERE userID = " . $_SESSION["userID"];
-	echo $sqlStr;
+	
 	$result = $conn->query($sqlStr);
 
 	if ($result->num_rows > 0) {
