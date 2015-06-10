6b8d0c08183d9a828ed77230b7beaf539ba91777
Chris <kipper19952000-stuff@yahoo.co.uk>
2015-06-10 08:33:03 +0100

    Created dayInfo page

---
+++ data/dayInfo.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+require "inc/functions.php";
+require "inc/connection.php";
+
+header('Content-Type: application/json');
+
+if (isset($_SESSION["userID"])){
+	$sqlStr = "SELECT * FROM tblUsers WHERE userID = " . $_SESSION["userID"];
+	
+	$result = $conn->query($sqlStr);
+
+	if ($result->num_rows > 0) {
+		$row = $result->fetch_assoc();
+		$json_array = array();
+		
+		$json_array["userID"] = $row["userID"];
+		$json_array["username"] = $row["username"];
+		$json_array["email"] = $row["email"];
+		$json_array["holiday"] = $row["holiday"];
+		
+		echo json_encode($json_array);
+	} else {
+		echo json_encode("no match");
+	}
+} else {
+	echo json_encode("Not logged in");
+}
+
+
+?>
--- data/userInfo.php
+++ data/userInfo.php
@@ -8,18 +8,28 @@ require "inc/connection.php";
 header('Content-Type: application/json');
 
 if (isset($_SESSION["userID"])){
-	$sqlStr = "SELECT * FROM tblUsers WHERE userID = " . $_SESSION["userID"];
+	$strWhere = "";
+	
+	if (isset($_GET['dateFrom'])){
+		$strWhere .= " AND date >= '" . $_GET['dateStart'] . "'";
+	}
+	
+	if (isset($_GET['dateTo'])){
+		$strWhere .= " AND date <= '" . $_GET['dateTo'] . "'";
+	}
+	
+	$sqlStr = "SELECT * FROM timeDetail WHERE userID = " . $_SESSION["userID"] . $strWhere;
 	
 	$result = $conn->query($sqlStr);
 
 	if ($result->num_rows > 0) {
-		$row = $result->fetch_assoc();
+		
 		$json_array = array();
 		
-		$json_array["userID"] = $row["userID"];
-		$json_array["username"] = $row["username"];
-		$json_array["email"] = $row["email"];
-		$json_array["holiday"] = $row["holiday"];
+		while ($row = $result->fetch_assoc()){
+			array_push($json_array, $row);
+		}
+		$row = $result->fetch_assoc();
 		
 		echo json_encode($json_array);
 	} else {
