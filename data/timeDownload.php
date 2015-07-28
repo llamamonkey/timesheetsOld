15899ccc74dbf1c5c2da2970241b95b4cca54b5b
Chris <kipper19952000-stuff@yahoo.co.uk>
2015-07-28 08:20:35 +0100

    Error check

--- data/timeDownload.php
+++ data/timeDownload.php
@@ -54,10 +54,10 @@ $objPHPExcel->getActiveSheet()->setTitle('Simple');
 $objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
 
 // We'll be outputting an excel file
-header('Content-type: application/vnd.ms-excel');
+//header('Content-type: application/vnd.ms-excel');
 
 // It will be called file.xls
-header('Content-Disposition: attachment; filename="file.xls"');
+//header('Content-Disposition: attachment; filename="file.xls"');
 
 // Write file to the browser
 $objWriter->save('php://output');
