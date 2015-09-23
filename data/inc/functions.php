214600f66b785e44670b3c57a2d781f7ead3383d
Chris <kipper19952000-stuff@yahoo.co.uk>
2015-09-23 08:32:07 +0100

    Disabled SSL Requirement

--- data/inc/functions.php
+++ data/inc/functions.php
@@ -3,8 +3,8 @@
 function requireSSL(){
 	if($_SERVER["HTTPS"] != "on" && $_SERVER["HTTPS"] != "1" && $_SERVER["HTTPS"] != 1)
 	{
-		header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-		exit();
+		//header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+		//exit();
 	}
 }
 
